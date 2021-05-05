0850bdb3c47d46c0f8a95c3fe8c45aee3dc66e6a
ksaketou <konstantinasaketou@yahoo.com>
2021-05-05 19:21:35 +0300

    Add test cases for the isPrime method

--- unittesting/src/test/java/math/MyMathTest.java
+++ unittesting/src/test/java/math/MyMathTest.java
@@ -1,6 +1,8 @@
 package math;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 /**
  * @author Konstantina Saketou
  *
@@ -8,30 +10,54 @@ import org.junit.Test;
 public class MyMathTest {
 
 	/**
-	 * Test method for {
-	 * 		@link math.MyMath#factorial(int)
-	 * }.
+	 * This is a test class for the class MyMath.
+	 * 
+	 * @author Konstantina Saketou
 	 */
 	
 	@Test (expected = IllegalArgumentException.class)
-	public void test_Factorial_thirteen() {
+	public void testFactorial_thirteen() {
 		System.out.println("Attempting to calculate factorial of thirteen.");
 		MyMath m = new MyMath();
 		m.factorial(13);
 	}
 
 	@Test (expected = IllegalArgumentException.class)
-	public void test_Factorial_minus_one() {
+	public void testFactorial_minus_one() {
 		System.out.println("Attempting to calculate factorial of minus one.");
 		MyMath m = new MyMath();
 		m.factorial(-1);
 	}
 	
 	@Test
-	public void test_Factorial_accepted() {
+	public void testFactorial_accepted() {
 		System.out.println("Attempting to calculate factorial of five.");
 		MyMath m = new MyMath();
 		assert 120 == m.factorial(5) : "Unable to calculate factorial of five.";
+	}
+	
+	@Test
+	public void testIsPrime_true() {
+		System.out.println("Checking if the input number is prime.");
+		MyMath m = new MyMath();
+		assert true == m.isPrime(5);
+	}
+	
+	@Test
+	public void testIsPrime_false() {
+		System.out.println("Checking if the input number is prime.");
+		MyMath m = new MyMath();
+		assert false == m.isPrime(6);
+	}
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void testIsPrime_incorrect() {
+		MyMath m = new MyMath();
+		thrown.expectMessage("Input number must be greater or equal to 2.");
+		m.isPrime(-1);
 	}
 
 }
