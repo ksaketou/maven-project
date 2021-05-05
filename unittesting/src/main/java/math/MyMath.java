0a5f22e3a0ea1b57b75f760fbe272050114bb04c
ksaketou <konstantinasaketou@yahoo.com>
2021-05-05 19:18:45 +0300

    Add isPrime method
    This commit adds to the MyMath class a new method called isPrime. This method checks if an
    input integer, greater than two, is prime and in that case it return true. Otherwise, it returns false.

--- unittesting/src/main/java/math/MyMath.java
+++ unittesting/src/main/java/math/MyMath.java
@@ -1,12 +1,12 @@
 package math;
 
-public class MyMath {
 /**
  * This class contains a function for the calculation of the factorial of
  * an integer input number.
  * 
  * @author Konstantina Saketou
  */
+public class MyMath {
 	
 	/**
 	 * This function gets an integer input value and calculates its factorial.
@@ -24,5 +24,30 @@ public class MyMath {
 			}
 			return f;
 		}
+	}
+	
+	/**
+	 * This function gets an integer input value and checks
+	 * if it is prime or not.
+	 * 
+	 * @param n an integer input
+	 * @return true if input is prime or false if not
+	 */
+	public boolean isPrime(int n) {
+		 
+		if (n < 2)
+	        throw new IllegalArgumentException("Input number must be greater or equal to 2.");
+	 
+	    else if (n == 2)
+	         return true;
+	 
+	    else if (n % 2 == 0)
+	         return false;
+	 
+	    for (int i = 3; i <= Math.sqrt(n); i += 2){
+	         if (n % i == 0)
+	             return false;
+	     }
+	   return true;
 	}
 }
